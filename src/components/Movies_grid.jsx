4d0e30a4d86a7017ed014f6749f7ceb923e8a2a8
elian gonzalez <elianngonzalez7@gmail.com>
2022-03-08 00:09:02 -0300

    scroll infinito funcional

--- src/components/Movies_grid.jsx
+++ src/components/Movies_grid.jsx
@@ -1,5 +1,4 @@
 import { useEffect , useState } from 'react';
-import { useQuery } from '../hooks/useQuery';
 import { get } from '../utils/httpClient';
 import { Moviecard } from './Moviecard';
 import style from './MoviesGrid.module.css';
@@ -8,25 +7,25 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 
 
 
-export function MoviesGrid () {
+export function MoviesGrid ({ search }) {
 
     const [movies, setMovies] = useState([]);
     const [Loading, setLoading] = useState(true);
     const [Page, setPage] = useState(1);
-
-    const query = useQuery();
-    const search = query.get('search');
+    const [hasmore, sethasMore] = useState(true);
     console.log(search);
 
     useEffect(() => {
         setLoading(true);
         const urlSearch = search 
-        ? `/search/movie?query=${search}&${Page} ` : '/discover/movie?page='+Page;
+        ? `/search/movie?query=${search}&page=${Page} ` : '/discover/movie?page=' + Page;
 
         get(urlSearch).then(data => {
-            setLoading(false);
             //setMovies(prevMovies => [...prevMovies, ...data.results]);
             setMovies(prevMovies => prevMovies.concat(data.results));
+            sethasMore(data.total_pages > data.page);
+            setLoading(false);
+
         });
     }, [search, Page]);
 
@@ -34,7 +33,7 @@ export function MoviesGrid () {
     return (
         <InfiniteScroll dataLength={movies.length} 
         next={()=>setPage((prevPage)=>prevPage + 1)} 
-        hasMore={true}
+        hasMore={hasmore}
         loader={<Spinner />}>
             <ul className={style.movieGrid}>
                 {movies.map((movie) => (
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,18 @@
 import { MoviesGrid } from "../components/Movies_grid";
 import { Search } from "../components/Search";
+import { useQuery } from "../hooks/useQuery";
 
 export function LandingPage () {
+    const query = useQuery();
+    const search = query.get('search');
+    //taje query para resetear los estados(useState) del moviegrid destruyendo el componente y volviendo a renderizarlo al 
+    //cambiar la key del componente (<MovieGrid />);
+    //pero tambien podria traer todos los estados(useState) del componente MoviesGrid al 
+    //LandingPage y resetearlos en el, pasando el estado como props() al componente MoviesGrid
+
     return (
         <div>
         <Search />
-        <MoviesGrid />
+        <MoviesGrid key={search} search={search} />
         </div>);
 }
