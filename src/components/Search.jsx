e39c9e361c82494e54bb3618171d786c8abd1f48
elian gonzalez <elianngonzalez7@gmail.com>
2022-03-07 21:42:28 -0300

    part 2 end

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,26 +5,27 @@ import { useHistory } from 'react-router-dom';
 import { useQuery } from '../hooks/useQuery';
 
 export function Search() {
-    const query = useQuery();
-    const search = query.get('search');
+	const query = useQuery();
+	const search = query.get('search');
 
-    const [SearchText, setSearchText] = useState("");
-    const history = useHistory();
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        history.push(`/?search=${SearchText}`)
-    };
+	const [SearchText, setSearchText] = useState("");
+	const history = useHistory();
 
-    useEffect(() => {
-        setSearchText(search || "" );
-    } , [search]);
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		history.push(`/?search=${SearchText}`)
+	};
 
-    return (
-        <div className={styles.searchContainer}>
-            <form className={styles.searchbox} onSubmit={handleSubmit} >
-                <input className={styles.inputSearch} type="text" value={SearchText} onChange={(e) => setSearchText(e.target.value)} placeholder="Search..." />
-                <button className={styles.buttonSearch} type="submit"><FaSearch size={20} /></button>
-            </form>
-        </div>
-    )
+	useEffect(() => {
+		setSearchText(search || "" );
+	} , [search]);
+
+	return (
+		<div className={styles.searchContainer}>
+			<form className={styles.searchbox} onSubmit={handleSubmit} >
+				<input className={styles.inputSearch} type="text" value={SearchText} onChange={(e) => setSearchText(e.target.value)} placeholder="Search..." />
+				<button className={styles.buttonSearch} type="submit"><FaSearch size={20} /></button>
+			</form>
+		</div>
+	)
 }
