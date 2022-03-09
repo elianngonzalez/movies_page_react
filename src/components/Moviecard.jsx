985d9583911ed0c71293da04e61043d010d3d0af
elian gonzalez <elianngonzalez7@gmail.com>
2022-03-09 00:03:15 -0300

    mostrar el placeholder si no encuentra poster

--- src/components/Moviecard.jsx
+++ src/components/Moviecard.jsx
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom';
 import style from './MovieCard.module.css';
+import noPoster from "../placeholder.webp"
 
 export function Moviecard({movie})  {
-    const imagenUrl = `https://image.tmdb.org/t/p/w400${movie.poster_path}` ;
+    const imagenUrl = movie.poster_path ? `https://image.tmdb.org/t/p/w400${movie.poster_path}` : noPoster ;
     return (
         
         <li className={style.movieCard}>
