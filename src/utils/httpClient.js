deb5b2d322e45b53d8685dd748809bd76f8769e8
elian gonzalez <elianngonzalez7@gmail.com>
2022-03-09 01:20:55 -0300

    variables de entorno

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,11 +1,12 @@
 
-const api = 'https://api.themoviedb.org/3'
+const API = process.env.REACT_APP_API;
+const API_TOKEN = process.env.REACT_APP_API_TOKEN;
 
 export function get ( path ) {
-    return fetch( api + path ,{
+    return fetch( API + path ,{
             headers:{
                 Authorization: 
-                'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMmFmZTFmMTVhY2FjMjUyY2VhZTMwODllMzk4YWVmYiIsInN1YiI6IjYyMTcxMTYxODhiYmU2MDA0Mjc1NThlOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.d4zcyNuzV5BrirNUfi3aICDP9m1H6SxAPA_xyfOz0MQ',
+                'Bearer ' + API_TOKEN ,
                 'Content-Type': 'application/json; charset=utf-8'
             }
         }).then(response => response.json())
