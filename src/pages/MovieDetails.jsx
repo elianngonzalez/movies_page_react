32ac294d0591d2a9a500a00eeb147825cd444969
elian gonzalez <elianngonzalez7@gmail.com>
2022-03-09 00:05:37 -0300

    obtener imagen por medio de una util

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -3,6 +3,8 @@ import { get } from '../utils/httpClient';
 import { useParams } from 'react-router-dom';
 import { useEffect , useState } from 'react';
 import { Spinner } from '../components/Spinner';
+import { getMovieImg } from '../utils/getMovieImg';
+
 
 
 
@@ -30,15 +32,17 @@ export function MovieDetails () {
         return null;
     }
 
-    const imagenUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path ;
+    const imagenUrl = getMovieImg(movie.poster_path, 400);
+
+    //const imagenUrl = movie.poster_path ? "https://image.tmdb.org/t/p/w300" + movie.poster_path : noPoster;
 
     return (
         <div className={styles.detailsContainer}>
             <img className={`${styles.col} ${styles.movieImage}`} src={imagenUrl} alt={movie.title} />
             <div className={`${styles.col} ${styles.movieDetails}`}>
                 <div><strong>Title:</strong> {movie.original_title}</div>
-                <p><strong>Description:</strong> {movie.overview}</p>
-                <div><strong>Genere:</strong> {movie.genres.map(gen => gen.name).join(", ")}</div>
+                <p><strong>Description:</strong> {movie.overview ? movie.overview : "no se econtro"}</p>
+                <div><strong>Genere:</strong> {movie.genres.map(genero => genero.name).join(", ")}</div>
             </div>
         </div>
     );
